6502299f0ff502e59b94716da4a89daf86637821
Dwight Gunning <dwight@dwightgunning.com>
2017-03-22 23:22:22 +0100

    Added react material-ui component library and a grid system

--- app/app.js
+++ app/app.js
@@ -15,6 +15,7 @@ import { Provider } from 'react-redux';
 import { applyRouterMiddleware, Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { useScroll } from 'react-router-scroll';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import 'sanitize.css/sanitize.css';
 
 // Import root app
@@ -66,19 +67,21 @@ const rootRoute = {
 
 const render = (messages) => {
   ReactDOM.render(
-    <Provider store={store}>
-      <LanguageProvider messages={messages}>
-        <Router
-          history={history}
-          routes={rootRoute}
-          render={
-            // Scroll to top when going to a new page, imitating default browser
-            // behaviour
-            applyRouterMiddleware(useScroll())
-          }
-        />
-      </LanguageProvider>
-    </Provider>,
+    <MuiThemeProvider>
+      <Provider store={store}>
+        <LanguageProvider messages={messages}>
+          <Router
+            history={history}
+            routes={rootRoute}
+            render={
+              // Scroll to top when going to a new page, imitating default browser
+              // behaviour
+              applyRouterMiddleware(useScroll())
+            }
+          />
+        </LanguageProvider>
+      </Provider>
+    </MuiThemeProvider>,
     document.getElementById('app')
   );
 };
--- app/containers/WritingPage/index.js
+++ app/containers/WritingPage/index.js
@@ -10,11 +10,14 @@ import Helmet from 'react-helmet';
 import {injectIntl, intlShape, defineMessages, FormattedMessage} from 'react-intl';
 import { createStructuredSelector } from 'reselect';
 
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import { Grid, Row, Col } from 'react-material-responsive-grid';
+
 import WordCount from 'components/WordCount';
 import LabelledTimestamp from 'components/LabelledTimestamp';
 import { changeWords } from './actions';
 import { makeSelectWritingPage, makeSelectWords, makeSelectWordCount, makeSelectStartTime, makeSelectMilestoneTime } from './selectors';
-import Textarea from './Textarea';
 import messages from './messages';
 
 export class WritingPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
@@ -46,18 +49,26 @@ export class WritingPage extends React.PureComponent { // eslint-disable-line re
             { name: 'description', content: 'Description of WritingPage' },
           ]}
         />
-        <div>
-          <FormattedMessage {...messages.header} />
-        </div>
-        <div>
-          <Textarea
-            id="words"
-            value={this.props.words}
-            onChange={this.props.onChangeWords} />
-        </div>
-        <WordCount count={this.props.wordCount} />
-        {startTime}
-        {milestoneTime}
+        <Grid fixed={'center'}>
+          <Row>
+            <Col xs4={4}>
+              <div>
+                <FormattedMessage {...messages.header} />
+              </div>
+              <Paper>
+                <TextField
+                  id="words"
+                  fullWidth={true}
+                  multiLine={true}
+                  value={this.props.words}
+                  onChange={this.props.onChangeWords} />
+              </Paper>
+              <WordCount count={this.props.wordCount} />
+              {startTime}
+              {milestoneTime}
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
